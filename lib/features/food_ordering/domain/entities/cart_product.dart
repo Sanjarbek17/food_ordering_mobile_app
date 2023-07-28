3406acd11649f4ad17f087363c66165bf66e4929
Sanjar17 <sanjarsaidov173@gmail.com>
2023-07-28 11:35:49 +0500

    entities created

---
+++ lib/features/food_ordering/domain/entities/cart_product.dart
@@ -0,0 +1,17 @@
+class CartProduct {
+  final String id;
+  final String title;
+  final int price;
+  final int quantity;
+  final int total;
+  final String thumbNail;
+
+  CartProduct({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.quantity,
+    required this.total,
+    required this.thumbNail,
+  });
+}
